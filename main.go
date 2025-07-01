b88eb946475e74a8114080fb0248b43372c6f5cf
Vladyslav <vladyslavhordieiev@gmail.com>
2025-07-01 09:33:34 +0300

    first commit

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type User struct {
 	ID      string
@@ -12,28 +15,84 @@ func (u *User) Deposit(amount float64) {
 	u.Balance += amount
 }
 
-func (u *User) Withdraw(amount float64) bool {
-	if u.Balance-amount < 0 {
-		return false
+func (u *User) Withdraw(amount float64) error {
+	if u.Balance < amount {
+		return errors.New("Insufficient funds")
 	} else {
 		u.Balance -= amount
-		return true
+		return nil
 	}
+}
+
+type Transaction struct {
+	FromID string
+	ToID   string
+	Amount float64
+}
+
+type PaymentSystem struct {
+	Users        map[string]*User
+	Transactions []Transaction
+}
+
+func (ps *PaymentSystem) AddUser(u *User) {
+	ps.Users[u.ID] = u
+}
+
+func (ps *PaymentSystem) AddTransaction(t Transaction) {
+	ps.Transactions = append(ps.Transactions, t)
+}
+
+func (ps *PaymentSystem) ProcessingTransactions(t Transaction) error {
+
+	if t.Amount < 1 {
+		return errors.New("Incorrect value")
+	}
+
+	fromUser, ok := ps.Users[t.FromID]
+	if !ok {
+		return errors.New("Sender user not found")
+	}
+	toUser, ok := ps.Users[t.ToID]
+	if !ok {
+		return errors.New("Receiver user not found")
+	}
+
+	err := fromUser.Withdraw(t.Amount)
+	if err != nil {
+		return err
+	}
+	toUser.Deposit(t.Amount)
+	return nil
 }
 
 func main() {
 
+	ps := &PaymentSystem{Users: make(map[string]*User), Transactions: []Transaction{}}
 	user1 := &User{ID: "1", Name: "Alice", Balance: 100.0}
-	user2 := &User{ID: "2", Name: "Bob", Balance: 200.0}
+	user2 := &User{ID: "2", Name: "Bob", Balance: 100.0}
 
-	user1.Deposit(100.1)
-	fmt.Printf("%s's balance after a deposit: %.2f\n", user1.Name, user1.Balance)
-	user2.Withdraw(199.9)
-	fmt.Printf("%s's balance after a withdraw: %.2f\n", user2.Name, user2.Balance)
-	user1.Withdraw(199.9)
-	fmt.Printf("%s's balance after a withdraw: %.2f\n", user1.Name, user1.Balance)
-	user2.Deposit(100)
-	fmt.Printf("%s's balance after a deposit: %.2f\n", user2.Name, user2.Balance)
+	ps.AddUser(user1)
+	ps.AddUser(user2)
 
-	fmt.Println(user1, user2)
+	t1 := Transaction{FromID: "1", ToID: "2", Amount: -1}
+	t2 := Transaction{FromID: "3", ToID: "1", Amount: 1000}
+	t4 := Transaction{FromID: "1", ToID: "2", Amount: 99}
+
+	ps.AddTransaction(t1)
+	ps.AddTransaction(t2)
+	ps.AddTransaction(t4)
+
+	for i, t := range ps.Transactions {
+		err := ps.ProcessingTransactions(t)
+		if err != nil {
+			fmt.Printf("Transaction number %d: %v\n", i+1, err)
+		} else {
+			fmt.Printf("Transaction number %d: Is successful\n", i+1)
+		}
+	}
+
+	fmt.Println("\nИтого:")
+	fmt.Printf("У первого пользователя получилось %.2f\n", ps.Users["1"].Balance)
+	fmt.Printf("У второго пользователя получилось %.2f\n", ps.Users["2"].Balance)
 }
